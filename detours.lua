3febf89e371368b3adc582ba7403268bb640c56a
Meepen <meepdarknessmeep@gmail.com>
2018-07-19 01:47:38 -0400

    more detours to make the code work further

--- detours.lua
+++ detours.lua
@@ -22,14 +22,28 @@ end
 
 -- create a fake io library
 io = {}
+local fs = js.global.localStorage
+-- don't check for updates
+fs["UpdateCheck.lua"] = ""
+-- don't run first run files
+fs["first.run"] = nil
 
 local FILE = {}
+local FILE_MT = {
+    __index = FILE
+}
 function FILE:close()
 end
 function FILE:flush()
 end
+function FILE:read(what)
+    if (what == "*a") then
+        return fs[self.path]
+    else
+        error("not implemented: "..what)
+    end
+end
 
-local fs = js.global.localStorage
 
 function io.open(fpath, mode)
     if (mode:find "r" and not fs[fpath]) then
@@ -38,5 +52,72 @@ function io.open(fpath, mode)
     return setmetatable({
         path = fpath,
         mode = mode
-    }, FILE);
+    }, FILE_MT);
 end
+
+-- 5.1 compat
+unpack = table.unpack
+bit = {
+    bor = function(val, ...)
+        for i = 2, select("#", ...) do
+            val = val | (select(i, ...))
+        end
+    end
+}
+--[[
+local _pairs = pairs
+
+-- repair regular pairs behavior from lua/luajit
+-- basically does ipairs first, then any other values
+local ipairs_aux = ipairs(t, nil)
+
+local function pairs_aux(t, k)
+    if (k == nil) then
+        return t.first_k, t.t[t.first_k]
+    else
+        local k = t.iter[k]
+        return k, t.t[k]
+    end
+end
+
+function pairs2(t)
+    local function n(val)
+        local t = val
+        if (not val.max) then
+            local k, v = ipairs_aux(t, val.last)
+            if (k) then
+                val.done[k] = true
+                val.last = k
+                return k, v
+            end
+            val.max = true
+            val.last = nil
+        end
+        local pairs_aux = _pairs(t, k)
+        local k, v = val.last
+        repeat
+            k, v = pairs_aux(t, val.last)    
+        until not k or not val.done[k]
+        val.last = k
+        return val, k
+    end
+
+    local last_k, first_k = {}
+    local start_k = last_k
+
+    local values = {
+        t = t,
+        iter = {}
+    }
+
+    for _, k in n, {
+        t = t,
+        last = 0,
+        done = {}
+    } do
+        values.iter[last_k] = k
+        last_k = k
+    end
+
+    return pairs_aux, values, start_k
+end]]
