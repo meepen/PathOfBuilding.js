7f83102d99b47dd4469975a6a50fcbd32cd01669
Meepen <meepdarknessmeep@gmail.com>
2018-07-21 08:04:55 -0400

    webgl beta(tm)

---
+++ renderer-gl.js
@@ -0,0 +1,550 @@
+var render = window.render = {
+    layer: 0,
+    subLayer: 0,
+    shaders: {},
+};
+
+var canvas = window.canvas = render.canvas = document.getElementById("gl");
+canvas.width = 1280;
+canvas.height = 720;
+
+canvas.addEventListener("mousemove", function mousemove(x) {
+    render.cx = x.clientX;
+    render.cy = x.clientY;
+});
+
+var ctx2d = document.createElement("canvas").getContext("2d");
+var gl = render.gl = canvas.getContext("webgl");
+
+render.InitShader = function InitShader(vsSource, fsSource) {
+    var gl = this.gl;
+
+    var vertexShader = this.LoadShader(gl.VERTEX_SHADER, vsSource);
+    var fragmentShader = this.LoadShader(gl.FRAGMENT_SHADER, fsSource);
+
+    var shaderProgram = gl.createProgram();
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+    gl.linkProgram(shaderProgram);
+
+
+    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+        alert("Unable to initialize the shader program: " + gl.getProgramInfoLog(shaderProgram));
+        return null;
+    }
+
+    return shaderProgram;
+}
+
+render.LoadShader = function LoadShader(type, source) {
+    var gl = this.gl;
+
+    var shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        alert("An error occurred compiling the shaders: " + gl.getShaderInfoLog(shader));
+        gl.deleteShader(shader);
+        return null;
+    }
+
+    return shader;
+}
+
+var basic = render.shaders["basic"] = render.InitShader(
+    `
+        attribute vec2 position;
+        uniform vec2 scale;
+
+        void main() {
+            gl_Position = vec4((position * scale - vec2(1.0, 1.0)) * vec2(1, -1), 1.0, 1.0);
+        }
+    `,
+    `
+        precision mediump float;
+        uniform vec4 color;
+        void main() {
+            gl_FragColor = color;
+        }
+    `
+);
+
+render.basic = {
+    program: basic,
+    attribLocations: {
+        position: gl.getAttribLocation(basic, "position"),
+    },
+    uniformLocations: {
+        scale: gl.getUniformLocation(basic, "scale"),
+        color: gl.getUniformLocation(basic, "color")
+    },
+};
+
+
+/*
+// we can rescale easily like this:
+canvas.style.width  = '128px';
+canvas.style.height = '72px';
+*/
+
+render.images = [];
+render.queue = [];
+
+render.Insert = function Insert(obj) {
+    obj.layer = this.layer;
+    obj.subLayer = this.subLayer;
+    obj.count = ++this.renderCount;
+    render.queue.push(obj);
+}
+
+function sorter(a, b) {
+    if (a.layer == b.layer) {
+        if (a.subLayer == b.subLayer) {
+            return b.count - a.count;
+        }
+        return b.subLayer - a.subLayer;
+    }
+    return b.layer - a.layer;
+}
+
+render.RunThread = function RunThread() {
+    if (!this.luaThread) {
+        this.luaThread = lua.lua_newthread(L);
+        lua.luaL_loadstring(this.luaThread, 'repeat runCallback("OnFrame") until coroutine.yield()');
+    }
+    var res;
+    res = lua.lua_resume(this.luaThread, 0, 0);
+
+    if (res === 2) // LUA_ERRRUN
+        throw new Error("render thread did not render successfully: " + lua.lua_tostring(L, -1));
+    else if (res !== 1)
+        throw new Error("render thread ended");
+
+    if (lua.lua_type(this.luaThread, 1) === 4) // LUA_TSTRING
+        throw new Error("unsupported render thread yield: " + lua.lua_tostring(this.luaThread, 1));
+}
+
+var rect = gl.createBuffer();
+render.RealDrawRect = function RealDrawRect(x, y, w, h, col) {
+    var gl = this.gl;
+    var shaderInfo = this.basic;
+    gl.bindBuffer(gl.ARRAY_BUFFER, rect);
+    gl.bufferData(
+        gl.ARRAY_BUFFER,
+        new Float32Array([
+            x, y,
+            x + w, y,
+            x, y + h,
+            x + w, y + h
+        ]),
+        gl.STATIC_DRAW
+    );
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, rect);
+    gl.vertexAttribPointer(
+        shaderInfo.attribLocations.position,
+        2,
+        gl.FLOAT,
+        false,
+        0,
+        0
+    );
+    gl.enableVertexAttribArray(shaderInfo.attribLocations.position);
+
+    gl.useProgram(shaderInfo.program);
+
+    gl.uniform2fv(shaderInfo.uniformLocations.scale, [2 / canvas.width, 2 / canvas.height]);
+    gl.uniform4fv(shaderInfo.uniformLocations.color, col);
+
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+}
+
+render.RealDrawString = function RealDrawString(text, x, y) {
+    var gl = this.gl;
+
+    for (var i = 0; i < text.length; i++) {
+        var tex = ctexture.GetTexture(gl, text, i);
+
+        gl.bindTexture(gl.TEXTURE_2D, obj.tex);
+
+        this.DrawRect(x, y, tex.w, tex.h);
+
+        x += tex.w;
+    }
+
+    gl.bindTexture(gl.TEXTURE_2D, null);
+}
+
+render.AdvanceFrame = function AdvanceFrame() {
+    this.renderCount = 0;
+    this.queue = [];
+
+    this.layer = 0;
+    this.subLayer = 0;
+
+    // call lua OnFrame
+    // render from queue
+    // TODO: not string
+    this.RunThread(L);
+    
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clearDepth(1.0);
+    gl.enable(gl.DEPTH_TEST);
+    gl.depthFunc(gl.LEQUAL);
+
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    var queue = this.queue;
+    this.last_queue = queue.slice()
+    queue.sort(sorter)
+
+    var offset_x = 0, offset_y = 0;
+
+    var last_color = [1, 1, 1, 1];
+
+    for (; queue.length;) {
+        var obj = queue.pop();
+
+        if (obj.color !== undefined && last_color !== obj.color) {
+            last_color = obj.color;
+        }
+
+        switch (obj.type) {
+            case "DrawRect":
+                this.RealDrawRect(obj.left + offset_x, obj.top + offset_y, obj.width, obj.height, last_color);
+                break;
+            /*case "SetDrawColor":
+                break;
+            case "DrawImage":
+                var imgEntry = this.images[obj.image];
+
+                if (!imgEntry || imgEntry.error) {
+                    var oldFillStyle = ctx.fillStyle;
+                    ctx.fillStyle ='pink';
+                    ctx.fillRect(obj.left + offset_x, obj.top + offset_y, obj.width, obj.height);
+                    ctx.fillStyle = oldFillStyle;
+                    break;
+                }
+
+                var img = imgEntry.image;
+
+                if ( obj.tcLeft && obj.tcTop && obj.tcRight && obj.tcBottom )
+                {
+                    var sx = img.width * obj.tcLeft;
+                    var sy = img.height * obj.tcTop;
+                    var sw = (img.width * obj.tcRight) - sx;
+                    var sh = (img.height * obj.tcBottom) - sy;
+
+                    // background hack - it's wrong
+                    if ( sw > img.width || sh > img.height )
+                    {
+                        ctx.drawImage(img, 0, 0, img.width, img.height, obj.left + offset_x, obj.top + offset_y, obj.width, obj.height);
+                    }
+                    else
+                    {
+                        ctx.drawImage(img, sx, sy, sw, sh, obj.left + offset_x, obj.top + offset_y, obj.width, obj.height);
+                    }
+                }
+                else
+                {
+                    ctx.drawImage(img, obj.left + offset_x, obj.top + offset_y, obj.width, obj.height);
+                }
+
+                break;
+            case "DrawImageQuad":
+                ctx.beginPath();
+                    ctx.moveTo(obj.x1, obj.y1);
+                    ctx.lineTo(obj.x2, obj.y2);
+                    ctx.lineTo(obj.x3, obj.y3);
+                    ctx.lineTo(obj.x4, obj.y4);
+                ctx.stroke();
+
+                break;
+
+            case "SetViewport":
+                var textBaseline = ctx.textBaseline;
+                var fillStyle = ctx.fillStyle;
+                var textAlign = ctx.textAlign;
+                var font = ctx.font;
+                offset_x = 0;
+                offset_y = 0;
+                ctx.restore();
+                ctx.save();
+                if (obj.x !== null) {
+                    ctx.beginPath();
+                    ctx.rect(obj.x, obj.y, obj.width, obj.height);
+                    ctx.clip();
+
+                    offset_x = obj.x;
+                    offset_y = obj.y;
+
+                    ctx.textBaseline = textBaseline;
+                    ctx.fillStyle = fillStyle;
+                    ctx.textAlign = textAlign;
+                    ctx.font = font;
+                }
+                break;
+
+            case "DrawString":
+                // TODO: separate _x crap
+                if ( obj.align == "LEFT" || obj.align == "LEFT_X" )
+                    ctx.textAlign = "left";
+                else if ( obj.align == "RIGHT" || obj.align == "RIGHT_X" )
+                    ctx.textAlign = "right";
+                else if ( obj.align == "CENTER" || obj.align == "CENTER_X" )
+                    ctx.textAlign = "center";
+
+                ctx.font = "" + (obj.height - 2) + "px " + (obj.font == "FIXED" ? "monospace" : "sans-serif");
+                ctx.textBaseline = "top";
+                var originalStyle = ctx.fillStyle;
+
+                var arr = this.StringToFormattedArray(obj.text);
+                var text_offset = 0;
+                
+                ctx.fillStyle = "rgba(255,255,255,1)";
+                for (var i = 0; i < arr.length; i++) {
+                    var part = arr[i];
+                    
+                    if (typeof part == "string") {
+                        ctx.fillText(part, obj.left + text_offset + offset_x, obj.top + offset_y);
+                        text_offset += ctx.measureText(part).width;
+                    }
+                    else {
+                        ctx.fillStyle = "rgba(" + (part.r * 255) + "," + (part.g * 255) + "," + (part.b * 255) + "," + (part.a) + ")";
+                    }
+                }
+
+                ctx.fillStyle = originalStyle;
+                break;
+
+*/
+            default:
+                //console.log("not implemented: " + obj.type);
+        }
+
+    }
+}
+
+var annoying_shit = [
+    { r: 0.0, g: 0.0, b: 0.0, a: 1.0 },
+    { r: 1.0, g: 0.0, b: 0.0, a: 1.0 },
+    { r: 0.0, g: 1.0, b: 0.0, a: 1.0 },
+    { r: 0.0, g: 0.0, b: 1.0, a: 1.0 },
+    { r: 1.0, g: 1.0, b: 0.0, a: 1.0 },
+    { r: 1.0, g: 0.0, b: 1.0, a: 1.0 },
+    { r: 0.0, g: 1.0, b: 1.0, a: 1.0 },
+    { r: 1.0, g: 1.0, b: 1.0, a: 1.0 },
+    { r: 0.69999999, g: 0.69999999, b: 0.69999999, a: 1.0 },
+    { r: 0.40000001, g: 0.40000001, b: 0.40000001, a: 1.0 },
+]
+
+render.ColorFromString = function ColorFromString(r, i) {
+    i = i || 0;
+    var g, b, a = 1;
+    if (r[0 + i] != '^') {
+        return;
+    }
+    var len
+    if (r[1 + i] == 'x') {
+        b = parseInt(r.slice(6 + i, 8 + i), 16) / 255;
+        g = parseInt(r.slice(4 + i, 6 + i), 16) / 255;
+        r = parseInt(r.slice(2 + i, 4 + i), 16) / 255;
+        len = 8;
+    }
+    else {
+        var obj = annoying_shit[parseInt(r.slice(1 + i, 2 + i))];
+        obj.len = 2;
+        return obj;
+    }
+
+    return {
+        r: r,
+        g: g,
+        b: b,
+        a: a,
+        len: len
+    }
+}
+
+render.StringToFormattedArray = function StringToFormattedArray(str) {
+    var ret = [];
+
+    var last_find = 0;
+
+    for (var i = 0; i < str.length; i++) {
+        var col = this.ColorFromString(str, i);
+        if (col) {
+            if (last_find != i) {
+                ret.push(str.slice(last_find, i));
+            }
+            ret.push(col);
+            last_find = (i += col.len);
+        }
+    }
+
+    if (i != last_find) {
+        ret.push(str.slice(last_find, i));
+    }
+
+    return ret;
+}
+
+render.SetDrawLayer = function(layer, subLayer) {
+    if (layer !== null)
+        this.layer = layer;
+    if (subLayer !== null)
+        this.subLayer = subLayer;
+    else
+        this.subLayer = 0;
+}
+
+render.SetViewport = function SetViewport(x, y, width, height) {
+    this.Insert({
+        x: x,
+        y: y,
+        width: width,
+        height: height,
+        type: "SetViewport"
+    });
+}
+
+render.SetDrawColor = function SetDrawColor(r, g, b, a) {
+    if (typeof r == "string") {
+        var col = render.ColorFromString(r);
+        r = col.r, g = col.g, b = col.b, a = col.a;
+    }
+    this.color = [r, g, b, a];
+}
+
+render.DrawRect = function DrawRect(left, top, width, height) {
+    this.Insert({
+        left: left,
+        top: top,
+        width: width,
+        height: height,
+        color: this.color,
+        type: "DrawRect"
+    });
+}
+
+render.DrawImage = function DrawImage(imgHandle, left, top, width, height, tcLeft, tcTop, tcRight, tcBottom) {
+    this.Insert({
+        image: imgHandle,
+        left: left,
+        top: top,
+        width: width,
+        height: height,
+        tcLeft: tcLeft, 
+        tcTop: tcTop,
+        tcRight: tcRight,
+        tcBottom: tcBottom,
+        color: this.color,
+        type: "DrawImage"
+    });
+}
+
+render.DrawImageQuad = function DrawImageQuad(imgHandle, x1, y1, x2, y2, x3, y3, x4, y4, s1, t1, s2, t2, s3, t3, s4, t4) {
+    this.Insert({
+        image: imgHandle,
+        x1: x1, y1: y1,
+        x2: x2, y2: y2,
+        x3: x3, y3: y3,
+        x4: x4, y4: y4,
+        s1: s1, t1: t1,
+        s2: s2, t2: t2,
+        s3: s3, t3: t3,
+        s4: s4, t4: t4,
+        type: "DrawImageQuad"
+    });
+}
+
+render.DrawString = function DrawString(left, top, align, height, font, text) {
+    this.Insert({
+        left: left,
+        top: top,
+        align: align,
+        height: height,
+        font: font,
+        text: text,
+        type: "DrawString"
+    });
+}
+
+render.DrawStringWidth = function DrawStringWidth(height, font, text) {
+    ctx2d.font = "" + (height - 2) + "px " + (font == "FIXED" ? "monospace" : "sans-serif");
+    return ctx2d.measureText(text).width;
+}
+
+render.DrawStringCursorIndex = function DrawStringCursorIndex() {
+    this.Insert({
+        type: "DrawStringCursorIndex"
+    });
+}
+
+render.LoadImage = function LoadImage(name) {
+    var obj = {
+        width: 0,
+        height: 0,
+        loaded: false,
+        error: false,
+        image: document.createElement("img"),
+    };
+
+    obj.image.onload = ( () => {
+        console.log( "ONLOAD!", name );
+        obj.width = obj.image.width;
+        obj.height = obj.image.height;
+        obj.loaded = true;
+    } );
+
+    obj.image.onerror = ( () => {
+        obj.loaded = true;
+        obj.error = true;
+    } );
+
+    obj.image.src = localStorage[name];
+
+    return this.images.push(obj) - 1;
+}
+
+render.IsImageLoaded = function IsImageLoaded(idx) {
+    return this.images[idx].loaded;
+}
+
+render.ImageWidth = function ImageWidth(idx) {
+    return this.images[idx].width;
+}
+
+render.ImageHeight = function ImageHeight(idx) {
+    return this.images[idx].height;
+}
+
+render.fileRequests = []
+
+var up_dir_hack = /[^\/]+\/\.\.\//g
+var newline_replace = /^#[^\n]+/;
+
+render.LoadFile = function LoadFile(name) {
+    var obj = {
+        loaded: false,
+        data: null,
+    }
+
+    fetch(name.replace(up_dir_hack, "")).then( res => {
+        res.text().then( data => {
+            obj.loaded = true;
+            obj.data = data.replace(newline_replace, "");
+        } );
+    } );
+
+    return this.fileRequests.push(obj) - 1;
+}
+
+render.IsFileLoaded = function IsFileLoaded(idx) {
+    return this.fileRequests[idx].loaded;
+}
+
+render.FileData = function FileData(idx) {
+    return this.fileRequests[idx].data;
+}
